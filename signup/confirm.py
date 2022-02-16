6f1f6370f074e23d6b55d921adb0ff2ec0b2ac0c
kg0r0 <kg0r0@yahoo.co.jp>
2022-02-16 23:58:41 +0900

    Add Confirmation Code

--- signup/confirm.py
+++ signup/confirm.py
@@ -1,19 +1,18 @@
 import os
 import boto3
-from getpass import getpass
-from dotenv import load_dotenv
+import json
+from dotenv import load_dotenv, find_dotenv
 
 dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
 load_dotenv(dotenv_path)
+
 client=boto3.client("cognito-idp", region_name=os.getenv("REGION_NAME"))
 username = input("[*] Enter Your Email Address: ")
-password = getpass("[*] Enter Your Password: ")
-
-response = client.sign_up(
+confirm_code = input("[*] Enter Your Confirmation Code: ")
+response = client.confirm_sign_up(
     ClientId=os.getenv("COGNITO_CLIENT_ID"),
     Username=username,
-    Password=password,
-    UserAttributes=[{"Name": "email", "Value": username}],
+    ConfirmationCode=confirm_code,
 )
 
-print(response)
+print(json.dumps(response))
