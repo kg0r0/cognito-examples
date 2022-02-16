ff4a90d42bc542761c7bf775b30bb4c27ca5da69
kg0r0 <kg0r0@yahoo.co.jp>
2022-02-17 00:09:05 +0900

    Add USER_PASSWORD_AUTH Flow

---
+++ USER_PASSWORD_AUTH/login.py
@@ -0,0 +1,21 @@
+import os
+import boto3
+from getpass import getpass
+from dotenv import load_dotenv
+
+dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
+load_dotenv(dotenv_path)
+client = boto3.client("cognito-idp", region_name=os.getenv("REGION_NAME"))
+username = input("[*] Enter Your Email Address: ")
+password = getpass("[*] Enter Your Password: ")
+
+response = client.initiate_auth(
+    ClientId=os.getenv("COGNITO_CLIENT_ID"),
+    AuthFlow="USER_PASSWORD_AUTH",
+    AuthParameters={"USERNAME": username, "PASSWORD": password},
+)
+print(response)
+
+access_token = response["AuthenticationResult"]["AccessToken"]
+response = client.get_user(AccessToken=access_token)
+print(response)
