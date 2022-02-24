2ea9f7ae1a0b34c31ff77e883811f36588a4dd46
kg0r0 <kg0r0@yahoo.co.jp>
2022-02-24 09:52:53 +0900

    Refactoring

--- oauth2-client/authorization-code/src/index.ts
+++ oauth2-client/authorization-code/src/index.ts
@@ -1,4 +1,4 @@
-import express, { ErrorRequestHandler } from 'express';
+import express from 'express';
 import session from 'express-session';
 import crypto from 'crypto';
 import { Issuer, TokenSet, generators } from 'openid-client';
@@ -19,8 +19,8 @@ declare module 'express-session' {
     codeVerifier: string;
     nonce: string;
     originalUrl: string;
-    authorized: boolean;
-    loggedIn: boolean;
+    isRedirected: boolean;
+    isLoggedIn: boolean;
   }
 }
 
@@ -37,8 +37,8 @@ app.use(session({
 /**
  * routes
  */
-app.get('/', async (req: express.Request, res: express.Response) => {
-  if (req.session.tokenSet) {
+app.get('/*', async (req: express.Request, res: express.Response) => {
+  if (req.session.isLoggedIn && req.session.isRedirected) {
     console.log('received and validated tokens %j', req.session.tokenSet);
     return res.send('OK');
   }
@@ -49,6 +49,21 @@ app.get('/', async (req: express.Request, res: express.Response) => {
     redirect_uris: [config.redirect_uri],
     response_types: ['code']
   })
+  if (req.session.isRedirected) {
+    const state = req.session.state;
+    const codeVerifier = req.session.codeVerifier;
+    const params = client.callbackParams(req);
+    const tokenSet = await client.callback(
+      config.redirect_uri,
+      params,
+      {
+        state,
+        code_verifier: codeVerifier
+      });
+    req.session.tokenSet = tokenSet;
+    req.session.isLoggedIn = true;
+    return res.redirect(req.session.originalUrl!);
+  }
   const state = generators.state();
   req.session.state = state;
 
@@ -63,33 +78,8 @@ app.get('/', async (req: express.Request, res: express.Response) => {
     code_challenge_method: 'S256',
   })
   req.session.originalUrl = req.originalUrl;
-  req.session.authorized = true;
+  req.session.isRedirected = true;
   return res.redirect(url);
-});
-
-app.get('/cb', async (req: express.Request, res: express.Response) => {
-  if (!req.session.authorized) {
-    return res.status(403).send('NG');
-  }
-  const issuer = await Issuer.discover(`https://cognito-idp.${config.region}.amazonaws.com/${config.poolid}`)
-  const client = new issuer.Client({
-    client_id: config.client_id,
-    client_secret: config.client_secret,
-    redirect_uris: [config.redirect_uri],
-    response_types: ['code']
-  })
-  const state = req.session.state;
-  const codeVerifier = req.session.codeVerifier;
-  const params = client.callbackParams(req);
-  const tokenSet = await client.callback(
-    config.redirect_uri,
-    params,
-    {
-      state,
-      code_verifier: codeVerifier
-    });
-  req.session.tokenSet = tokenSet;
-  return res.redirect(req.session.originalUrl!);
 });
 
 app.listen(PORT, () => {
