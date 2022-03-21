81eb8f70155e551a19f5cf15169bb237f49e8231
kg0r0 <kg0r0@yahoo.co.jp>
2022-03-21 13:59:17 +0900

    Refactoring

--- amplify-js/oauth-flow-app/src/App.tsx
+++ amplify-js/oauth-flow-app/src/App.tsx
@@ -1,7 +1,6 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Amplify, { Auth, Hub } from 'aws-amplify';
 import awsconfig from './aws-exports';
-import './App.css';
 
 Amplify.configure(awsconfig);
 
@@ -19,37 +18,29 @@ function App() {
           setUser(null);
           break;
         case 'signIn_failure':
-          console.log('Sign in failure', data);
-          break;
         case 'cognitoHostedUI_failure':
           console.log('Sign in failure', data);
           break;
       }
     });
+
     getUser().then(userData => setUser(userData));
   }, []);
 
-  const getUser = async () => {
-    try {
-      const userData = await Auth.currentAuthenticatedUser();
-      Auth.currentSession().then((data) => {
-        console.log(`token: ${data.getIdToken().getJwtToken()}`);
-      });
-      console.log(userData);
-      return userData;
-    } catch (e) {
-      return console.log('Not signed in');
-    }
+  function getUser() {
+    return Auth.currentAuthenticatedUser()
+      .then(userData => userData)
+      .catch(() => console.log('Not signed in'));
   }
 
-  return user ? (
+  return (
     <div>
-      <p>username: {user.username}</p>
-      <button onClick={() => Auth.signOut()}>Sign Out</button>
-    </div>
-  ) : (
-    <div>
-      <button onClick={() => Auth.federatedSignIn()}>Sign In</button>
+      <p>User: {user ? JSON.stringify(user.attributes) : 'None'}</p>
+      {user ? (
+        <button onClick={() => Auth.signOut()}>Sign Out</button>
+      ) : (
+        <button onClick={() => Auth.federatedSignIn()}>Federated Sign In</button>
+      )}
     </div>
   );
 }
