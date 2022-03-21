36fad7a721871a7a0fd3eed05f23d9a71546b08e
kg0r0 <kg0r0@yahoo.co.jp>
2022-03-21 21:10:52 +0900

    Add USER_PASSWORD_AUTH React App

---
+++ amplify-js/user-password-auth/src/aws-exports.ts
@@ -0,0 +1,12 @@
+const Auth = {
+  // Amazon Cognito Region
+  region: 'XX-XXXX-X',
+  // Amazon Cognito User Pool ID
+  userPoolId: 'XX-XXXX-X_abcd1234',
+  // Amazon Cognito Web Client ID (26-char alphanumeric string)
+  userPoolWebClientId: 'a1b2c3d4e5f6g7h8i9j0k1l2m3',
+  // Manually set the authentication flow type. Default is 'USER_SRP_AUTH'
+  authenticationFlowType: 'USER_PASSWORD_AUTH',
+}
+
+export default Auth;
