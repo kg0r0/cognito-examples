a4211b234c0e667062a6472cf7b51cf0917f6b7e
kg0r0 <kg0r0@yahoo.co.jp>
2022-04-08 20:23:22 +0900

    Add revoke_token example

---
+++ sdk-python/revoke_token.py
@@ -0,0 +1,50 @@
+import os
+import boto3
+from getpass import getpass
+from dotenv import load_dotenv
+
+dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
+load_dotenv(dotenv_path)
+
+REGION_NAME = os.getenv("REGION_NAME")
+CLIENT_ID = os.getenv("CLIENT_ID")
+USER_POOL_ID = os.getenv("USER_POOL_ID")
+
+client = boto3.client("cognito-idp", region_name=REGION_NAME)
+username = input("[*] Enter Your Email Address: ")
+password = getpass("[*] Enter Your Password: ")
+response = client.initiate_auth(
+    ClientId=CLIENT_ID,
+    AuthFlow="USER_PASSWORD_AUTH",
+    AuthParameters={"USERNAME": username, "PASSWORD": password},
+)
+access_token = response["AuthenticationResult"]["AccessToken"]
+refresh_token = response["AuthenticationResult"]["RefreshToken"]
+assert access_token is not None
+assert refresh_token is not None
+print("[*] Issued Access Token and Refresh Token")
+
+assert client.initiate_auth(
+    ClientId=CLIENT_ID,
+    AuthFlow="REFRESH_TOKEN_AUTH",
+    AuthParameters={"USERNAME": username, "REFRESH_TOKEN": refresh_token},
+)["ResponseMetadata"]["HTTPStatusCode"] == 200
+print("[*] Successful Refresh Token verification")
+
+result = client.admin_user_global_sign_out(
+    UserPoolId=USER_POOL_ID,
+    Username=username
+)
+assert result["ResponseMetadata"]["HTTPStatusCode"] == 200 
+print("[*] Successful revoke of Refresh Token")
+
+try:
+    client.initiate_auth(
+        ClientId=CLIENT_ID,
+        AuthFlow="REFRESH_TOKEN_AUTH",
+        AuthParameters={"USERNAME": username, "REFRESH_TOKEN": refresh_token},
+    )
+except client.exceptions.NotAuthorizedException as e:
+    assert "Refresh Token has been revoked" in str(e)
+    print("[*] Refresh Token has been revoked")
+print("[*] All done!")
