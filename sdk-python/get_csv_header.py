7767901e1179dd96a78ecfc9308fe361908b0f9c
kg0r0 <kg0r0@yahoo.co.jp>
2022-04-15 23:30:02 +0900

    Add get_csv_header example

---
+++ sdk-python/get_csv_header.py
@@ -0,0 +1,11 @@
+import os
+import boto3
+from dotenv import load_dotenv
+
+dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
+load_dotenv(dotenv_path)
+client = boto3.client("cognito-idp", region_name=os.getenv("REGION_NAME"))
+response = client.get_csv_header(
+    UserPoolId=os.getenv("USER_POOL_ID"),
+)
+print(response)
