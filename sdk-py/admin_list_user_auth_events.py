d3b79b1d21895de28bee959e9891b393711ed392
kg0r0 <kg0r0@yahoo.co.jp>
2022-05-01 21:42:13 +0900

    Add admin_list_user_auth_events example

---
+++ sdk-py/admin_list_user_auth_events.py
@@ -0,0 +1,14 @@
+import os
+import boto3
+from dotenv import load_dotenv
+
+dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
+load_dotenv(dotenv_path)
+client = boto3.client("cognito-idp", region_name=os.getenv("REGION_NAME"))
+username = input("[*] Enter Your Email Address: ")
+
+response = client.admin_list_user_auth_events(
+    UserPoolId=os.getenv("USER_POOL_ID"),
+    Username=username,
+)
+print(response)
