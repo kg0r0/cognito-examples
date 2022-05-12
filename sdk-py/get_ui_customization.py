1d9ffd11719d591889995c8e06046f1eb8e72bc2
kg0r0 <kg0r0@yahoo.co.jp>
2022-05-12 19:48:21 +0900

    Add get_ui_customization example

---
+++ sdk-py/get_ui_customization.py
@@ -0,0 +1,12 @@
+import os
+import boto3
+from dotenv import load_dotenv
+dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
+load_dotenv(dotenv_path)
+client = boto3.client("cognito-idp", region_name=os.getenv("REGION_NAME"))
+
+response = client.get_ui_customization(
+    UserPoolId=os.getenv("USER_POOL_ID"),
+    ClientId=os.getenv("CLIENT_ID"),
+)
+print(response)
