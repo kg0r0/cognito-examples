2914541df4737b936322c5f16938ce60d401fd21
kg0r0 <kg0r0@yahoo.co.jp>
2022-05-16 00:59:17 +0900

    Add ListUsers.php

---
+++ sdk-php/ListUsers.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Copyright 2010-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * This file is licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License. A copy of
+ * the License is located at
+ *
+ * http://aws.amazon.com/apache2.0/
+ *
+ * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ *  ABOUT THIS PHP SAMPLE: This sample is part of the SDK for PHP Developer Guide topic at
+ * https://docs.aws.amazon.com/sdk-for-php/v3/developer-guide/iam-examples-working-with-policies.html
+ *
+ */
+
+require 'vendor/autoload.php';
+
+use Aws\CognitoIdentityProvider\CognitoIdentityProviderClient;
+use Aws\Exception\AwsException;
+
+
+/**
+ * 
+ *
+ * This code expects that you have AWS credentials set up per:
+ * https://docs.aws.amazon.com/sdk-for-php/v3/developer-guide/guide_credentials.html
+ */
+
+ 
+$client = new CognitoIdentityProviderClient([
+    'profile' => 'default',
+    'region' => '<region>',
+    'version' => '2016-04-18'
+]);
+
+$userPoolId = "<user pool id>";
+
+try {
+    $result = $client->listUsers([
+        'UserPoolId' => $userPoolId, 
+    ]);
+    
+    var_dump($result);
+} catch (AwsException $e) {
+    // output error message if fails
+    echo $e->getMessage() . "\n";
+    error_log($e->getMessage());
+}
