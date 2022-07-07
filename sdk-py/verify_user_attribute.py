25d6c5591c558a3ba0c842f0f3e8df43576042f2
kg0r0 <kg0r0@yahoo.co.jp>
2022-07-07 10:54:50 +0900

    Add verify_user_attribute example

---
+++ sdk-py/verify_user_attribute.py
@@ -0,0 +1,25 @@
+import os
+import boto3
+from getpass import getpass
+from dotenv import load_dotenv
+
+dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
+load_dotenv(dotenv_path)
+client = boto3.client("cognito-idp", region_name=os.getenv("REGION_NAME"))
+username = input("[*] Enter Your Email Address: ")
+password = getpass("[*] Enter Your Password: ")
+
+response = client.initiate_auth(
+    ClientId=os.getenv("CLIENT_ID"),
+    AuthFlow="USER_PASSWORD_AUTH",
+    AuthParameters={"USERNAME": username, "PASSWORD": password},
+)
+
+code = input("[*] Enter verification code: ")
+access_token = response["AuthenticationResult"]["AccessToken"]
+result = client.verify_user_attribute(
+    AccessToken=access_token,
+    AttributeName='email',
+    Code=code
+)
+print(result)
