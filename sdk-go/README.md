dab4eeff7c4e11b140084880f893552da0c786a8
kg0r0 <kg0r0@yahoo.co.jp>
2023-10-07 21:31:07 +0900

    Add InitiateAuth example

---
+++ sdk-go/README.md
@@ -0,0 +1,7 @@
+# AWS SDK for Go code examples
+
+This is code examples that demonstrate how to use the AWS SDK for Go.
+
+## References
+- https://docs.aws.amazon.com/sdk-for-go/api/service/cognitoidentityprovider/
+- https://pkg.go.dev/github.com/aws/aws-sdk-go-v2/service/cognitoidentityprovider
---
+++ sdk-go/initiateauth.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/cognitoidentityprovider"
+	"github.com/aws/aws-sdk-go-v2/service/cognitoidentityprovider/types"
+)
+
+func main() {
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("<region>"))
+	if err != nil {
+		log.Fatalf("unable to load SDK config, %v", err)
+	}
+
+	var ClientId = "<clientid>"
+
+	svc := cognitoidentityprovider.NewFromConfig(cfg)
+	resp, err := svc.InitiateAuth(context.TODO(), &cognitoidentityprovider.InitiateAuthInput{
+		AuthFlow:       types.AuthFlowTypeUserPasswordAuth,
+		AuthParameters: map[string]string{"USERNAME": "<username>", "PASSWORD": "<password>"},
+		ClientId:       &ClientId,
+	})
+	if err != nil {
+		log.Fatalf("%+v\n", err)
+	}
+	log.Printf("%+v\n", resp.AuthenticationResult)
+}
